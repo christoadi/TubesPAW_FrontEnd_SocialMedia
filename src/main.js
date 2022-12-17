7cc44a4e63b12318a5f5fec28e333527fcf51702
christoadi <chrizto.adi456@gmail.com>
2022-12-17 21:34:49 +0700

    Set API Hosting

--- src/main.js
+++ src/main.js
@@ -5,14 +5,16 @@ import router from './router';
 import axios from 'axios';
 import { BootstrapVue } from "bootstrap-vue";
 
-Vue.use(BootstrapVue);
+// Vue.use(BootstrapVue);
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue);
 Vue.prototype.$http = axios;
-Vue.prototype.$api = 'http://127.0.0.1:8000/api';
+Vue.prototype.$api = 'https://backend.sosmedkuu.my.id/api'; //api hosting
+// Vue.prototype.$api = 'http://127.0.0.1:8000/api'; //api localhost
 
 new Vue({
   vuetify,
