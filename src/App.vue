d90a759eec761c10287357758d549d22de684e43
christoadi <chrizto.adi456@gmail.com>
2022-12-17 21:34:06 +0700

    Add new Font

--- src/App.vue
+++ src/App.vue
@@ -10,3 +10,11 @@ export default {
 }
 </script>
 
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700;800&display=swap');
+
+#app {
+  font-family: 'Montserrat', sans-serif !important;
+}
+</style>
+
